a15ab5a297ea79adbb3a657d27f5e1d54821afb8
Ryo Nakamura <r7kamura@gmail.com>
2022-06-18 11:44:19 +0900

    Add `Sevencop/OrderField` cop

---
+++ lib/rubocop/cop/sevencop/order_field.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Sevencop
+      # Identifies a String including "field" is passed to `order` or `reorder`.
+      #
+      # @safety
+      #   This cop is unsafe because it can register a false positive.
+      #
+      # @example
+      #
+      #   # bad
+      #   articles.order('field(id, ?)', a)
+      #
+      #   # good
+      #   articles.order(Arel.sql('field(id, ?)'), a)
+      #
+      #   # bad
+      #   reorder('field(id, ?)', a)
+      #
+      #   # good
+      #   reorder(Arel.sql('field(id, ?)'), a)
+      #
+      class OrderField < Base
+        extend AutoCorrector
+
+        MSG = 'Wrap safe SQL String by `Arel.sql`.'
+
+        RESTRICT_ON_SEND = %i[
+          order
+          reorder
+        ].freeze
+
+        ORDER_METHOD_NAMES = RESTRICT_ON_SEND.to_set.freeze
+
+        def_node_matcher :order_with_field?, <<~PATTERN
+          (send
+            _ ORDER_METHOD_NAMES
+            (str /field\(.+\)/)
+            ...
+          )
+        PATTERN
+
+        # @param [RuboCop::AST::SendNode] node
+        def on_send(node)
+          return unless order_with_field?(node)
+
+          first_argument_node = node.arguments.first
+          add_offense(first_argument_node) do |corrector|
+            corrector.replace(
+              node.arguments.first,
+              "Arel.sql(#{first_argument_node.source})"
+            )
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/sevencop/order_field_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Sevencop::OrderField, :config do
+  context 'without field' do
+    it 'registers no offense' do
+      expect_no_offenses(<<~TEXT)
+        articles.order('id DESC')
+      TEXT
+    end
+  end
+
+  context 'with Hash' do
+    it 'registers no offense' do
+      expect_no_offenses(<<~TEXT)
+        articles.order(id: :desc)
+      TEXT
+    end
+  end
+
+  context 'with receiver' do
+    it 'autocorrects offense' do
+      expect_offense(<<~TEXT)
+        articles.order('field(id, ?)', a)
+                       ^^^^^^^^^^^^^^ Wrap safe SQL String by `Arel.sql`.
+      TEXT
+
+      expect_correction(<<~RUBY)
+        articles.order(Arel.sql('field(id, ?)'), a)
+      RUBY
+    end
+  end
+
+  context 'without receiver' do
+    it 'autocorrects offense' do
+      expect_offense(<<~TEXT)
+        order('field(id, ?)', a)
+              ^^^^^^^^^^^^^^ Wrap safe SQL String by `Arel.sql`.
+      TEXT
+
+      expect_correction(<<~RUBY)
+        order(Arel.sql('field(id, ?)'), a)
+      RUBY
+    end
+  end
+
+  context 'with reorder' do
+    it 'autocorrects offense' do
+      expect_offense(<<~TEXT)
+        reorder('field(id, ?)', a)
+                ^^^^^^^^^^^^^^ Wrap safe SQL String by `Arel.sql`.
+      TEXT
+
+      expect_correction(<<~RUBY)
+        reorder(Arel.sql('field(id, ?)'), a)
+      RUBY
+    end
+  end
+end
