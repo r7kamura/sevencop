c857f0674742cc9017881e32f8539a5d90c754a2
Ryo Nakamura <r7kamura@gmail.com>
2022-08-20 09:13:58 +0900

    Refactor config injection logic

--- lib/sevencop.rb
+++ lib/sevencop.rb
@@ -3,7 +3,7 @@
 require 'pathname'
 require 'yaml'
 
-require_relative 'sevencop/inject'
+require_relative 'sevencop/rubocop_extension'
 require_relative 'sevencop/version'
 
 require_relative 'rubocop/cop/sevencop/belongs_to_optional'
@@ -14,15 +14,3 @@ require_relative 'rubocop/cop/sevencop/redundant_existence_check'
 require_relative 'rubocop/cop/sevencop/to_s_with_argument'
 require_relative 'rubocop/cop/sevencop/uniqueness_validator_explicit_case_sensitivity'
 require_relative 'rubocop/cop/sevencop/where_not'
-
-module Sevencop
-  PROJECT_ROOT = ::Pathname.new(__dir__).parent.expand_path.freeze
-
-  CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
-
-  CONFIG = ::YAML.safe_load(CONFIG_DEFAULT.read).freeze
-
-  private_constant(:CONFIG_DEFAULT, :PROJECT_ROOT)
-end
-
-Sevencop::Inject.defaults!
---
+++ lib/sevencop/rubocop_extension.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+require_relative 'config_loader'
+
+RuboCop::ConfigLoader.instance_variable_set(
+  :@default_configuration,
+  Sevencop::ConfigLoader.call
+)
