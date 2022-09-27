0199714690704eaebb339a16fb3d827059bb9345
Ryo Nakamura <r7kamura@gmail.com>
2022-09-27 11:37:44 +0900

    Rename `Sevencop/MethodDefinitionKeywordArgumentsOrdered` to `Sevencop/MethodDefinitionKeywordArgumentOrdered`
    The singular form is more consistent with the other cops.

--- spec/rubocop/cop/sevencop/method_definition_keyword_argument_ordered_spec.rb
+++ spec/rubocop/cop/sevencop/method_definition_keyword_argument_ordered_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe RuboCop::Cop::Sevencop::MethodDefinitionKeywordArgumentsOrdered, :config do
+RSpec.describe RuboCop::Cop::Sevencop::MethodDefinitionKeywordArgumentOrdered, :config do
   context 'when keyword arguments are sorted' do
     it 'does not register an offense' do
       expect_no_offenses(<<~RUBY)
