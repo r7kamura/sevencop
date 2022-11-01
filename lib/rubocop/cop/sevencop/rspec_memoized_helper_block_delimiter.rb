c9399b097e89af074e9839df97642b518cb08b6b
Ryo Nakamura <r7kamura@gmail.com>
2022-11-02 08:44:59 +0900

    Add `Sevencop/RSpecMemoizedHelperBlockDelimiter` cop

---
+++ lib/rubocop/cop/sevencop/rspec_memoized_helper_block_delimiter.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+require 'set'
+
+module RuboCop
+  module Cop
+    module Sevencop
+      # Use do-end block delimiter on RSpec memoized helper.
+      #
+      # Since these helpers are in effect method definitions, they should be written in the same style as `def`.
+      #
+      # @example
+      #   # bad
+      #   let(:foo) { 'bar' }
+      #
+      #   # good
+      #   let(:foo) do
+      #     'bar'
+      #   end
+      #
+      #   # bad
+      #   subject(:foo) { 'bar' }
+      #
+      #   # good
+      #   subject(:foo) do
+      #     'bar'
+      #   end
+      class RSpecMemoizedHelperBlockDelimiter < Base
+        extend AutoCorrector
+
+        include ConfigurableEnforcedStyle
+        include RangeHelp
+
+        MSG = 'Use do-end block delimiter on RSpec memoized helper.'
+
+        # @param node [RuboCop::AST::BlockNode]
+        # @return [void]
+        def on_block(node) # rubocop:disable InternalAffairs/NumblockHandler
+          return unless bad?(node)
+
+          add_offense(
+            node.location.begin.with(
+              end_pos: node.location.end.end_pos
+            )
+          ) do |corrector|
+            autocorrect(corrector, node)
+          end
+        end
+
+        private
+
+        # @!method memoized_helper?(node)
+        #   @param node [RuboCop::AST::Node]
+        #   @return [Boolean]
+        def_node_matcher :memoized_helper?, <<~PATTERN
+          (block
+            (send
+              nil?
+              {:let :subject}
+              ...
+            )
+            ...
+          )
+        PATTERN
+
+        # @param corrector [RuboCop::Cop::Corrector]
+        # @param node [RuboCop::AST::BlockNode]
+        # @return [void]
+        def autocorrect(
+          corrector,
+          node
+        )
+          corrector.insert_before(node.location.begin, ' ') unless whitespace_before?(node.location.begin)
+          corrector.replace(node.location.begin, 'do')
+          corrector.replace(node.location.end, 'end')
+          wrap_in_newlines(corrector, node) if node.single_line?
+        end
+
+        # @param node [RuboCop::AST::BlockNode]
+        # @return [Boolean]
+        def bad?(node)
+          memoized_helper?(node) && node.braces?
+        end
+
+        # @param range [Parser::Source::Range]
+        # @return [Boolean]
+        def whitespace_before?(range)
+          range.source_buffer.source[range.begin_pos - 1].match?(/\s/)
+        end
+
+        # @param corrector [RuboCop::Cop::Corrector]
+        # @param node [RuboCop::AST::BlockNode]
+        # @return [void]
+        def wrap_in_newlines(
+          corrector,
+          node
+        )
+          corrector.wrap(
+            node.location.begin.with(
+              begin_pos: node.location.begin.end_pos,
+              end_pos: node.location.end.begin_pos
+            ),
+            "\n",
+            "\n"
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/sevencop/rspec_memoized_helper_block_delimiter_spec.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Sevencop::RSpecMemoizedHelperBlockDelimiter, :config do
+  context 'when do-end is used' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        let(:foo) do
+          'bar'
+        end
+      RUBY
+    end
+  end
+
+  context 'when blockpass is used' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        let(:foo, &block)
+      RUBY
+    end
+  end
+
+  context 'when braces is used on `let`' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        let(:foo) {
+                  ^ Use do-end block delimiter on RSpec memoized helper.
+          'bar'
+        }
+      RUBY
+
+      expect_correction(<<~RUBY)
+        let(:foo) do
+          'bar'
+        end
+      RUBY
+    end
+  end
+
+  context 'when braces is used on `subject` without name' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        subject {
+                ^ Use do-end block delimiter on RSpec memoized helper.
+          'bar'
+        }
+      RUBY
+
+      expect_correction(<<~RUBY)
+        subject do
+          'bar'
+        end
+      RUBY
+    end
+  end
+
+  context 'when braces is used on `subject` with name' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        subject(:foo) {
+                      ^ Use do-end block delimiter on RSpec memoized helper.
+          'bar'
+        }
+      RUBY
+
+      expect_correction(<<~RUBY)
+        subject(:foo) do
+          'bar'
+        end
+      RUBY
+    end
+  end
+
+  context 'when braces is used in one-line' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        let(:foo) { 'bar' }
+                  ^^^^^^^^^ Use do-end block delimiter on RSpec memoized helper.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        let(:foo) do
+         'bar'#{' '}
+        end
+      RUBY
+    end
+  end
+
+  context 'when braces is used in without spaces' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        let(:foo){'bar'}
+                 ^^^^^^^ Use do-end block delimiter on RSpec memoized helper.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        let(:foo) do
+        'bar'
+        end
+      RUBY
+    end
+  end
+end
