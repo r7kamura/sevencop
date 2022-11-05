1d836ad766d89783e64ab0c4609b0b61ba3da4b1
Ryo Nakamura <r7kamura@gmail.com>
2022-11-05 09:22:19 +0900

    Add `Sevencop/RSpecRailsResponseParsedBody` cop

---
+++ spec/rubocop/cop/sevencop/rspec_rails_response_parsed_body_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Sevencop::RSpecRailsResponseParsedBody, :config do
+  context 'when `response.parsed_body` is used' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        expect(response.parsed_body).to eq('foo' => 'bar')
+      RUBY
+    end
+  end
+
+  context 'when `JSON.parse(response.body)` is used' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        expect(JSON.parse(response.body)).to eq('foo' => 'bar')
+               ^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `response.parsed_body` to `JSON.parse(response.body)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        expect(response.parsed_body).to eq('foo' => 'bar')
+      RUBY
+    end
+  end
+
+  context 'when `::JSON.parse(response.body)` is used' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        expect(::JSON.parse(response.body)).to eq('foo' => 'bar')
+               ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `response.parsed_body` to `JSON.parse(response.body)`.
+      RUBY
+
+      expect_correction(<<~RUBY)
+        expect(response.parsed_body).to eq('foo' => 'bar')
+      RUBY
+    end
+  end
+end
